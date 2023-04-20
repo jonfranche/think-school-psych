0cb4b9c48dc018a035f722fbaae232fc4f2bb32a
Jonathan Franche <jonfranche@gmail.com>
2023-04-20 16:54:15 -0700

    Updated NewBlog with form data

--- frontend/src/pages/YourStories/NewBlog.js
+++ frontend/src/pages/YourStories/NewBlog.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import "./NewBlog.css";
 
+// const reducer = (state, action) => {
+//   if (action.type === "create-blog") {
+//     return {
+
+//     }
+//   }
+// }
+
 const NewBlog = () => {
+  // const [state, dispatch] = useReducer(reducer, {
+  //   id: 0,
+  //   date: "",
+  //   userId: 1,
+  //   commentsIds: [],
+  //   title: "",
+  //   text: ""
+  // })
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // dispatch({type: "create-blog", title: event.target.});
+    const form = event.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
   return (
     <div className="new-blog">
       <h2>New Story</h2>
-      <form className="new-blog-form">
+      <form className="new-blog-form" onSubmit={submitHandler}>
         <label htmlFor="blog-title">Title: </label>
         <input name="blog-title" type="text" />
         <label htmlFor="blog-text">Story: </label>
