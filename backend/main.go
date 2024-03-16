9bab30d2983671cf3fe0d92abacab8f0ead4e985
Jonathan Franche <jonfranche@gmail.com>
2024-03-16 08:59:47 -0700

    Added one TODO about CORS

--- backend/main.go
+++ backend/main.go
@@ -31,17 +31,8 @@ func init() {
 func main() {
 	a := App {}
 	a.Initialize(dbUser, dbPassword, dbPort, dbHost, dbName)
+
+	// TODO: enable CORS
+
 	a.Run(":8010")
-	// Set up Server
-	// r := gin.Default()
-	// r.GET("/api/stories", controllers.GetStories)
-	// r.POST("/api/stories/new", controllers.PostStory)
-	// r.PATCH("/api/stories/edit/:id", controllers.UpdateStory)
-	// r.DELETE("/api/stories/edit/:id", controllers.DeleteStory)
-	// r.GET("/api/stories/:id", controllers.GetStoryByID)
-	// r.GET("/api/comments/:id", controllers.GetCommentByID)
-	// r.POST("/api/comments/", controllers.PostComment)
-	// r.PATCH("/api/comments/:id", controllers.UpdateComment)
-	// r.DELETE("/api/comments/:id", controllers.DeleteComment)
-	// r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
