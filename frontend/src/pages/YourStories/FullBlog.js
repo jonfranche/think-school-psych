fe98da1cc74b36de5cf7b871c4db88bc56b27f24
Jonathan Franche <jonfranche@gmail.com>
2024-04-08 23:44:29 -0700

    linked frontend to /api/stories/{id} endpoint

--- frontend/src/pages/YourStories/FullBlog.js
+++ frontend/src/pages/YourStories/FullBlog.js
@@ -4,76 +4,101 @@ import { useParams } from "react-router-dom";
 import Button from "../../shared/components/UIElements/Button";
 import Comment from "./components/Comment";
 import NewComment from "./components/NewComment";
-import { DUMMY_BLOGS, DUMMY_USERS } from "../../DummyData";
 
 import "./FullBlog.css";
 
 const FullBlog = (props) => {
+  const [blogData, setBlogData] = useState();
+  const [loading, setLoading] = useState(false);
   const [showNewComment, setShowNewComment] = useState(false);
   let { id } = useParams();
   // convert id param to be a number
-  id = Number(id);
 
-  const blogData = DUMMY_BLOGS.find((blog) => blog.id === id);
-  // console.log(blogData);
   useEffect(() => {
-    // console.log(`/stories/${id}`);
-  }, [id]);
+    const getBlogData = () => {
+      return fetch(`/api/stories/${id}`, { method: "GET" })
+        .then((response) => {
+          const respData = response.json();
+          return respData;
+        })
+        .then((response) => {
+          setBlogData(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    setTimeout(() => {
+      getBlogData();
+      setLoading(true);
+    }, 1000);
+  }, []);
 
   const getAuthor = () => {
-    const author = DUMMY_USERS.filter((user) => user.id === blogData.userId);
-    return author[0].name;
+    // TODO: add username to response json
+    return "Author";
+    // const author = DUMMY_USERS.filter((user) => user.id === blogData.userId);
+    // return author[0].name;
   };
 
   const addCommentButtonHandler = () => {
     setShowNewComment(!showNewComment);
   };
 
+  // TODO: add comments amounts to resopnse data
+
   return (
-    <div className="full-blog">
-      <div className="full-blog-header">
-        <h3>{blogData.title}</h3>
-        <div className="full-blog-header-sub-title">
-          <span>{"by " + getAuthor()}</span>
-          <span>{" " + blogData.date.toLocaleDateString()}</span>
+    <React.Fragment>
+      {!loading && <h4>Loading...</h4>}
+      {blogData && (
+        <div className="full-blog">
+          <div className="full-blog-header">
+            <h3>{blogData.title}</h3>
+            <div className="full-blog-header-sub-title">
+              <span>{"by " + getAuthor()}</span>
+              <span>{" " + new Date(blogData.date).toLocaleDateString()}</span>
+            </div>
+          </div>
+          <div className="full-blog-body">
+            <p>{blogData.text}</p>
+          </div>
+          <div className="full-blog-footer">
+            <span>0 Comments</span>
+            <Button
+              link={true}
+              to={`../stories/edit/${id}`}
+              state={{ title: blogData.title, text: blogData.text }}
+            >
+              Edit Story
+            </Button>
+            {!showNewComment && (
+              <button
+                className="full-blog-footer__add-comment"
+                onClick={addCommentButtonHandler}
+              >
+                Add Comment
+              </button>
+            )}
+          </div>
+          <div className="comments" id="comment-section">
+            {showNewComment && (
+              <NewComment
+                visible={showNewComment}
+                setVisible={addCommentButtonHandler}
+                blogId={blogData.id}
+                userId={blogData.userID}
+              />
+            )}
+            {/* {blogData.commentsIds.map((comment) => (
+             <Comment key={comment} id={comment} blogId={blogData.id}/>
+           ))} */}
+          </div>
         </div>
-      </div>
-      <div className="full-blog-body">
-        <p>{blogData.text}</p>
-      </div>
-      <div className="full-blog-footer">
-        <span>{blogData.commentsIds.length + " Comments"}</span>
-        <Button
-          link={true}
-          to={`../stories/edit/${id}`}
-          state={{ title: blogData.title, text: blogData.text }}
-        >
-          Edit Story
-        </Button>
-        {!showNewComment && (
-          <button
-            className="full-blog-footer__add-comment"
-            onClick={addCommentButtonHandler}
-          >
-            Add Comment
-          </button>
-        )}
-      </div>
-      <div className="comments" id="comment-section">
-        {showNewComment && (
-          <NewComment
-            visible={showNewComment}
-            setVisible={addCommentButtonHandler}
-            blogId={id}
-            userId={1}
-          />
-        )}
-        {blogData.commentsIds.map((comment) => (
-          <Comment key={comment} id={comment} blogId={blogData.id}/>
-        ))}
-      </div>
-    </div>
+      )}
+    </React.Fragment>
   );
 };
 
 export default FullBlog;
+
