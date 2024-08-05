79e6675ae1cb1d6cb31bc3622c3be376792f3340
Jonathan Franche <jonfranche@gmail.com>
2024-08-05 10:31:32 -0700

    Updated postgres.env to prod and dev versions

--- backend/main.go
+++ backend/main.go
@@ -16,7 +16,7 @@ var (
 )
 
 func init() {
-	err := godotenv.Load("../env/postgres.env")
+	err := godotenv.Load("../env/postgres_prod.env")
 	if err != nil {
 		log.Fatal(".env file couldn't be loaded")
 	}
