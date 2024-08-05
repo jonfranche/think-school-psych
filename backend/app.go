418b8dd21a0f6a8fb281e090904455f3c3fecf17
Jonathan Franche <jonfranche@gmail.com>
2024-08-05 10:27:55 -0700

    Enabled cors in app.go

--- backend/app.go
+++ backend/app.go
@@ -478,6 +478,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "localhost:8080")
 	w.WriteHeader(code)
 	w.Write(response)
 }
